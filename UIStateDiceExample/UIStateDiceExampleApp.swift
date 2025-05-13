4f8f46288528b2a508036d75deff07f455d36d72
adarista012 <ad.arista012@gmail.com>
2025-05-13 12:30:57 -0400

    Initial Commit

---
+++ UIStateDiceExample/UIStateDiceExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIStateDiceExampleApp.swift
+//  UIStateDiceExample
+//
+//  Created by antonio dante arista rivas on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct UIStateDiceExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UIStateDiceExampleTests/UIStateDiceExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  UIStateDiceExampleTests.swift
+//  UIStateDiceExampleTests
+//
+//  Created by antonio dante arista rivas on 5/13/25.
+//
+
+import Testing
+
+struct UIStateDiceExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
